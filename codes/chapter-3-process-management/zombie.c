03e2bcddd4363b137f49f3d59ce87169d5e5af69
Alamin <alamin.ineedahelp@gmail.com>
2020-06-14 11:04:11 +0600

    zombie code fixed

--- codes/chapter-3-process-management/zombie.c
+++ codes/chapter-3-process-management/zombie.c
@@ -5,15 +5,20 @@
 
 int main() {
 	pid_t child_pid;
+	int status;
 	child_pid = fork();
+
 	if(child_pid < 0) {
 		printf("Fork Error!\n");
 	} else if (child_pid == 0) {
 		exit(0);
 	} else {
-		wait();
 		printf("Zombie\n");
-		sleep(100);
+		sleep(10);
+        child_pid = wait(&status);
+        if (WIFEXITED(status))
+            fprintf(stderr, "\n\t[%d]\tProcess %d exited with status %d.\n",
+                (int) getpid(), child_pid, WEXITSTATUS(status));
 		exit(0);
 	}
 }
